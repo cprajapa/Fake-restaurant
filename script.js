9cf42197170302ae7570ccd5f3ca75cbe63d6156
Chirag Prajapati <86082806+cprajapa@users.noreply.github.com>
2023-01-03 13:36:23 -0700

    Final
    Signed-off-by: Chirag Prajapati <86082806+cprajapa@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-audioLink = document.getElementById("audioLink");
-musicButton = document.getElementById("musicButton");
+var html = document.getElementById("html");
+var body = document.getElementById("body");
+var loaderDiv = document.createElement("div");
+var table = document.getElementById("table");
+var audioLink = document.getElementById("audioLink");
+var musicButton = document.getElementById("musicButton");
 
-loaderDiv = document.createElement("div");
+var css = document.styleSheets[0];
 
-loaderDiv.style.opacity = "1";
+var twoSec = 2000;
+
+var delay = setTimeout(loadingAnimation, twoSec);
+
+function loadingAnimation() {
+    "use strict";
+    loaderDiv.remove();
+    html.style.backgroundColor = "beige";
+    table.style.opacity = "1";
+}
+
+function toggleMusic() {
+    "use strict";
+    if (audioLink.paused) {
+        audioLink.play();
+        audioLink.paused = false;
+        musicButton.innerHTML = "Pause: Coffee Shop Music";
+    } else {
+        audioLink.pause();
+        audioLink.paused = true;
+        musicButton.innerHTML = "Play: Coffee Shop Music";
+    }
+}
+
 loaderDiv.style.width = "120px";
 loaderDiv.style.height = "120px";
 loaderDiv.style.position = "fixed";
@@ -14,35 +41,13 @@ loaderDiv.style.top = "50%";
 loaderDiv.style.left = "50%";
 loaderDiv.style.borderRadius = "50%";
 
-css = window.document.styleSheets[0];
-
 css.insertRule(`@keyframes spin {
     to {
         transform: rotate(360deg);
-    }}`);
+    }
+   }
+`);
 
-document.body.appendChild(loaderDiv);
+body.appendChild(loaderDiv);
 
-document.onload(setTimeout(function() {
-	loaderDiv.style.opacity = "0";
-}, 1500))
-
-// function afterLoad() {
-//     setTimeout(function () {
-//         let body = document.body
-//         body.style.opacity = "1";
-//     }, 1500);
-// }
-function toggleMusic() {
-
-	if (audioLink.paused) {
-		audioLink.play();
-		audioLink.paused = false;
-		musicButton.innerHTML = "Pause: Coffee Shop Music";
-	}
-	else {
-		audioLink.pause();
-		audioLink.paused = true;
-		musicButton.innerHTML = "Play: Coffee Shop Music";
-	}
-}
+document.onload(delay);
