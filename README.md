28ddac0f4ecb357ffec725143500399d9e8d32e4
Chirag Prajapati <86082806+cprajapa@users.noreply.github.com>
2023-01-03 10:13:18 -0700

    Updated

--- README.md
+++ README.md
@@ -1,25 +1,30 @@
 # Directions
 
-Create a web page for a fake restaurant using JavaScript and Media (videos and sound). You may create a different type of page, but please check with the teacher first to make sure it meets the requirements.
+Create a web page for a fake restaurant using JavaScript and Media (videos and sound). You may create a different type
+of page, but please check with the teacher first to make sure it meets the requirements.
 
 ### Assessment Levels
-Start at developing and work your way up. To pass the course you only need to demonstrate a developing level of understanding.
+
+Start at developing and work your way up. To pass the course you only need to demonstrate a developing level of
+understanding.
 
 Any missing items will move you down the assessment continuum.
 
 **Developing**
+
 - Create at least one onclick function for your page
 - Use JavaScript to trigger an animation
 - Use a video in your page
-  
+
 **Proficient**
+
 - Create an onload function for your page and add a loading animation
 - Create at least one onclick function for your page
 - Use JavaScript to trigger an animation
 - Use a video in your page
 
-  
 **Exemplary**
+
 - Create an onload function for your page and add a loading animation
 - Create at least one onclick function for your page
 - Use an if, else if statement in your JavaScript code (Hint: maybe use this to turn something on and off)
--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-function removeLoader() {
-    loaderDiv = document.getElementById("loaderDiv");
-    loaderDiv.remove();
+tr = document.getElementById("menuRow")
+
+about = document.createElement("td");
+aboutText = document.createTextNode("About")
+about.appendChild(aboutText);
+about.style.left = "30%";
+
+tr.appendChild(about);
+
+menu = document.createElement("td");
+menuText = document.createTextNode("Menu")
+menu.appendChild(menuText);
+menu.style.left = "45%";
+
+tr.appendChild(menu);
+
+shop = document.createElement("td");
+shopText = document.createTextNode("Shop")
+shop.appendChild(shopText);
+shop.style.left = "60%";
+
+tr.appendChild(shop);
+
+contact = document.createElement("td");
+contactText = document.createTextNode("Contact")
+contact.appendChild(contactText);
+contact.style.left = "75%";
+
+tr.appendChild(contact);
+
+function onLoad() {
+    setTimeout(function () {
+        let loaderDiv = document.getElementById("loaderDiv")
+        loaderDiv.remove();
+    }, 3000);
 }
-function onDivLoad() {
-    setTimeout(removeLoader, 1500);
-}
-function afterLoading() {
-	
+
+// function afterLoad() {
+//     setTimeout(function () {
+//         let body = document.body
+//         body.style.opacity = "1";
+//     }, 1500);
+// }
+function toggleMusic() {
+    let audioLink = document.getElementById("audioLink");
+    console.log("audioLink.paused = " + audioLink.paused);
+
+    if (audioLink.paused) {
+        audioLink.play();
+        audioLink.paused = false;
+    } else {
+        audioLink.pause();
+        audioLink.paused = true;
+    }
 }
